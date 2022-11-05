16715a64b8ec2ca9ec06714a164cd77da4e45300
Kshitiz Bharati <kshbharati@outlook.com>
2022-11-05 14:01:36 +1100

    Added Next 13 setting and Integrated Dialogflow Web view

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Page() {
+    return (
+        <>
+            <div>BuywithUs</div>
+            <iframe
+                allow="microphone;"
+                width="350"
+                height="430"
+                src="https://console.dialogflow.com/api-client/demo/embedded/114795bd-6431-4b1b-b107-c05d397543f6"
+            ></iframe>
+        </>
+    );
+}
