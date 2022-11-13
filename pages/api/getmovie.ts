ea5d4ba528713db16aa0a922ff72844269a07d03
Kshitiz Bharati <kshbharati@outlook.com>
2022-11-13 14:26:17 +1100

    Added Card response

--- pages/api/getmovie.ts
+++ pages/api/getmovie.ts
@@ -7,22 +7,32 @@ import { User } from '@prisma/client';
 const prisma = new PrismaClient();
 
 
-type Data = {
-  fulfillmentText: string,
-  source:string
-}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
       // res.status(200).json(await users())
     res.status(200).json({
-        fulfillmentText:"Movie",
-        source: "getmovie",
-    })
+        fulfillmentMessages: [
+            {
+                card: {
+                    title: "card title",
+                    subtitle: "card text",
+                    imageUri:
+                        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic01.nyt.com%2Fimages%2F2011%2F01%2F14%2Farts%2F14MOVING-span%2FMOVING-jumbo.jpg&f=1&nofb=1&ipt=d992525934df99e762a810588c789b5e503404666ccb977d5465ddf8e75c2e95&ipo=images",
+                    buttons: [
+                        {
+                            text: "button text",
+                            postback:
+                                "https://example.com/path/for/end-user/to/follow",
+                        },
+                    ],
+                },
+            },
+        ],
+    });
 }
-
 
 //Testing environmentz
 const createMany = async()=> await prisma.user.createMany({
