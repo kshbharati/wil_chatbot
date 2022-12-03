2a71c18766bded72da6e82b12248818f2ccd9216
Kshitiz Bharati <kshbharati@outlook.com>
2022-12-03 17:23:31 +1100

    Added tailwindcss support. Changed Home layout

---
+++ app/property/[pid]/page.tsx
@@ -0,0 +1,4 @@
+
+export default function Page({params}) {
+    return <div>{params.pid}</div>;
+}
---
+++ components/Menu/chatButton.tsx
@@ -0,0 +1,34 @@
+
+'use client'
+import { MouseEventHandler, useEffect } from "react";
+
+export default function ChatButton() {
+    return (
+        <div
+            className="md:flex items-center hidden space-x-4 ml-8 lg:ml-12"
+        >
+            <button
+                onClick={()=>{
+
+                        console.warn("This is the place");
+                        let chatbot=document.getElementById("chatbot");
+                        if(chatbot){
+                            chatbot.setAttribute(
+                                "class",
+                                "visible fixed chatbot bottom-0 right-0 z-50 pr-8 pb-8"
+                            );
+                        }
+
+                        let chatBotFrame=document.getElementById("chatBotFrame");
+                        if(chatBotFrame){
+                            chatBotFrame.setAttribute('src',chatBotFrame.hasAttribute('data-src') ? chatBotFrame.getAttribute('data-src') : "");
+                        }
+                        console.log(chatbot);
+                }}
+                className="text-text-gray-600 btn py-2 hover:cursor-pointer px-4 rounded text-white bg-gradient-to-tr from-indigo-600 to-green-600 hover:shadow-lg"
+            >
+                CHAT NOW
+            </button>
+        </div>
+    );
+};
