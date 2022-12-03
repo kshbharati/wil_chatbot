e5746d3c3b9c04e99abaa1da3dcee84a8b05355a
Kshitiz Bharati <kshbharati@outlook.com>
2022-12-03 17:32:08 +1100

    Fixed Params type

--- app/property/[pid]/page.tsx
+++ app/property/[pid]/page.tsx
@@ -1,4 +1,7 @@
 
-export default function Page({params}) {
+// interface ParametersForProperty{
+//     pid:string
+// }
+export default function Page(params:{pid:string}) {
     return <div>{params.pid}</div>;
 }
