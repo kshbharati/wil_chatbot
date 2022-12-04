59710fb9129906a0e0f6a9380017821f92dfbebf
Kshitiz Bharati <kshbharati@outlook.com>
2022-12-04 17:18:11 +1100

    Fixed Few Layouts and Added Property Page Rendering

--- app/property/[pid]/page.tsx
+++ app/property/[pid]/page.tsx
@@ -1,16 +1,72 @@
+import {Property, Address} from 'prisma'
+import Image from 'next/image'
+import { PropertyImages, PropertyInformation } from '@prisma/client';
 
-interface ParametersForProperty{
-    pid:string
+const imageLoader=(src:string)=>{
+    return src;
 }
-export default function Page(params:any) {
-    if(params?.pid)
-    {
-        return <div>{params.pid}</div>;
+
+export default async function Page({params}) {
+    let link="http://localhost:3000/api/property/"+params.pid;
+
+    const res = await fetch(link);
+    let property:Property= await res.json();
+    if(property){
+        let propertyImg:PropertyImages=property.propertyImages[0];
+        let address:Address=property.address;
+        let propertyInfo:PropertyInformation= property.propertyInformation;
+
+        return (
+            <>
+                <div>
+                    <p>{property.id}</p>
+                    <p>{property.name}</p>
+                    <p>{property.createdAt}</p>
+                    <p>{property.updatedAt}</p>
+                </div>
+
+                <Image
+                    src={imageLoader(propertyImg.imageLink)}
+                    alt={propertyImg.imageDescription}
+                    height="500"
+                    width="500"
+                />
+                <div>
+                    <p>Address</p>
+                    <p>{address.suburb}</p>
+                    <p>{address.postcode}</p>
+                </div>
+
+                <div>
+                    <p>Information</p>
+                    <p>{propertyInfo.propertyType}</p>
+                    <p>{property.floorSize} m3</p>
+                </div>
+            </>
+        );
+    }
+
+    return(
+        <>
+            {params.pid}
+        </>
+    );
+
+
+}
+
+export async function getProperty(pid:any){
+    let link="http://localhost:3000/api/property/";
+    if(pid){
+        link+=pid;
+    }
+    try {
+        const res = await fetch(link);
+        return (await res.json());
+    } catch (error) {
+        console.trace(error);
+        return error;
     }
 
 
 }
-
-// export async function getStaticProps(params:any){
-
-// }
--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -1,14 +1,17 @@
 export default function ChatBotComponent(){
-    return(
-            <div id="chatbot" className="invisible fixed chatbot bottom-0 right-0 z-50 pr-8 pb-8">
-                <iframe name="chatbotDialogflow" title="DialogFlow" id="chatBotFrame"
-                    allow="microphone;"
-                    width="350"
-                    height="430"
-                    data-src="https://console.dialogflow.com/api-client/demo/embedded/114795bd-6431-4b1b-b107-c05d397543f6"
-                    src=""
-                ></iframe>
-            </div>
+    return (
+        <div
+            id="chatbot"
+            className="invisible fixed chatbot bottom-0 right-0 z-50 pr-8 pb-8"
+        >
+            <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+            <df-messenger
+                intent="WELCOME"
+                chat-title="buywithusBot"
+                agent-id="55c3f215-87ec-427c-81ce-78723cd755fc"
+                language-code="en"
+            ></df-messenger>
+        </div>
     );
 }
 
