29ac1afd428c24633c356eb92682d04fa5a58b65
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-22 14:23:51 +1100

    Added session id and user id generator using uusid

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -1,5 +1,6 @@
 import Script from "next/script";
 import * as React from 'react'
+import * as uuid from 'uuid'
 declare global {
     namespace JSX {
         interface IntrinsicElements {
@@ -21,11 +22,16 @@ export default function ChatBotComponent(){
             <Script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></Script>
             <df-messenger
                 intent="WELCOME"
-                chat-title="buywithusBot"
+                chat-title="Buy With Us - Rodhe"
                 agent-id="55c3f215-87ec-427c-81ce-78723cd755fc"
                 language-code="en"
+                session-id={generateSessionId()}
+                user-id={generateSessionId()}
             ></df-messenger>
         </div>
     );
 }
 
+function generateSessionId(){
+    return uuid.v4();
+}
