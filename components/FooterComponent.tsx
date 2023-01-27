994901ac42ea01a52ce1870f607a568432cfad0f
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-27 22:24:07 +1100

    Added policy links to FooterComponent

---
+++ components/FooterComponent.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function FooterComponent() {
+    return (
+        <div className="flex bottom-0 w-full p-6 space-x-20 bg-slate-800 text-white text-center place-content-center">
+            <div className="">
+                <Link href="/privacy-policy">Privacy Policy</Link>
+            </div>
+            <div className="">
+                <Link href="/sitemap">Sitemap</Link>
+            </div>
+        </div>
+    );
+}
