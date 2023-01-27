00537d5be8a4a8024ec2939c94c9653f515a1e4f
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-27 23:13:34 +1100

    readjusted link

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -4,7 +4,7 @@ export default function FooterComponent() {
     return (
         <div className="flex bottom-0 w-full p-6 space-x-20 bg-slate-800 text-white text-center place-content-center">
             <div className="">
-                <Link href="/privacy-policy">Privacy Policy</Link>
+                <Link href="/privacy">Privacy Policy</Link>
             </div>
             <div className="">
                 <Link href="/sitemap">Sitemap</Link>
