5094824463d1481780b5287d4008cd243a2ccf8a
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-27 22:21:43 +1100

    Added sitemap page

---
+++ app/sitemap/page.tsx
@@ -0,0 +1,9 @@
+import SliderComponent from "@components/Featured/SliderComponent";
+
+export default function sitemap() {
+    return (
+        <>
+            <p>Sitemap</p>
+        </>
+    );
+}
