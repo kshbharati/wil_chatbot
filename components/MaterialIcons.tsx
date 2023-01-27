0b4bd7fee1ca5cd6ca931e4b113c9639ae3f5f00
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-27 22:24:35 +1100

    Collugated MaterialIcons to allow SSR using client

---
+++ components/MaterialIcons.tsx
@@ -0,0 +1,8 @@
+'use client';
+import HomeIcon from "@mui/icons-material/Home";
+import LocationCityRoundedIcon from "@mui/icons-material/LocationCityRounded";
+import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
+import PhoneAndroidRoundedIcon from "@mui/icons-material/PhoneAndroidRounded";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+
+export {HomeIcon,LocationCityRoundedIcon,LocalPhoneRoundedIcon,PhoneAndroidRoundedIcon,EmailRoundedIcon}
