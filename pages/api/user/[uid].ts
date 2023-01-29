36fbadc10ba50945c4a3de18b33cd8ed026010da
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-29 15:35:01 +1100

    readjusted unused imports

--- pages/api/user/[uid].ts
+++ pages/api/user/[uid].ts
@@ -1,34 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import {User, Prisma, PrismaClient, ChatbotEnquiry} from '@prisma/client'
-import { PrismaContext } from "@prismaContext";
-
-const prisma = new PrismaClient();
-
-type UserApiRequest = NextApiRequest & {
-    id:number;
-}
-
-interface UserId{
-  id:number
-}
-
-// //Read users
-// async function  user(userId:any):Promise<User | null>{
-//     return await prisma.user.findUnique({
-//         where: {
-//             id: userId,
-//         },
-//     });
-// }
-
-//http://localhost/api/user/claf8ct4p0000dwqjpz5sqo12
+import { PrismaContext } from "prisma/prismaContext";
 
 export default async function userHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
     const {uid} = req.query;
-    const user: User| null= await prisma.user.findUnique({
+    if(!uid)
+    {
+      res.status(403).json({message:"Forbidden"});
+      return;
+    }
+    const user: User| null= await PrismaContext.user.findUnique({
         where: {
             id: uid as string,
         },
@@ -37,6 +21,10 @@ export default async function userHandler(
         }
     });
 
+    if(!user){
+      res.status(404).json({message:"Not Found"});
+      return;
+    }
     const enquiries: ChatbotEnquiry[] =
         await PrismaContext?.chatbotEnquiry.findMany();
 
