5cc888df4d31d5bd17695aba58e710fead1e7186
Kshitiz Bharati <kshbharati@outlook.com>
2023-01-29 20:30:02 +1100

    Renamed purpose of test

--- __test__/pages/api/property/featured.test.ts
+++ __test__/pages/api/property/featured.test.ts
@@ -2,7 +2,7 @@ import FeaturedHandler from "pages/api/property/featured";
 import testHandler from "__test__/pages/api/TestHandler"
 
 describe("Property Get", () => {
-    it("should forbid with empty query", async () => {
+    it("show featured properties", async () => {
         const res = await testHandler(FeaturedHandler, {
             method: "GET",
         });
